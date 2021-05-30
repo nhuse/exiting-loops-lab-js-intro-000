a7b5a7cac536459166e21274817a8725089695fa
Nathan Huse <nathan.huse@yahoo.com>
2021-05-30 14:10:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
-    if (array[i] === findFn.toString()) {
+    if (array[i] === findFn {
       return array[i];
     }
   }
