7f18b3eab53b12ce3e4ccb0d148fce8733e4cc79
Nathan Huse <nathan.huse@yahoo.com>
2021-05-30 13:50:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const evens = [0,2,4,6,8,10,12,14,16,18,20];
-
-
 function breakOut(array, changeValue,stopValue) {
-
+  for (var i = 0; i < array.length; i++) {
+    if(array[i] === stopValue){
+      break;
+    }
+    array[i] === changeValue;
+  }
+  return array;
 }
